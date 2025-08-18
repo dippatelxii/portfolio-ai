45b1d3ad35d58b772fdc185b06156b3a04452931
dippatelxii <dippatel1207@gmail.com>
2025-08-18 18:15:22 +0530

    updated index.ts file

--- data/index.ts
+++ data/index.ts
@@ -8,17 +8,17 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "I focus on building reliable AI systems, grounded in measurable results. ",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/b1.jpg",
     spareImg: "",
   },
   {
     id: 2,
-    title: "I'm very flexible with time zone communications",
+    title: "I work beyond borders, not bound by clocks.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
