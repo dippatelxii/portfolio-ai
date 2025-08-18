d6040fb72ff09f15c68def8a69e63b4e1e7fb6f0
dippatelxii <dippatel1207@gmail.com>
2025-08-18 17:40:10 +0530

    added tech stack

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["LangChain", "LlamaIndex", "LangGraph"];
+  const rightLists = ["TensorFlow", "Pytorch", "Keras"];
 
   const [copied, setCopied] = useState(false);
 
